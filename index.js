e0a412a5acd0bab3ac9dd0f76b1e71c058161041
ChuckBTaylor <WLScripps@gmail.com>
2017-10-08 14:46:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance){
+  return distance > 2500 ? "No can do." : distance <= 400 ? "This one is on me!" : "I will gladly take your thirty bucks."
+};
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+};
+
+function switchOnCharmFromTip(tip){
+  return tip === 'generous' ? "Thank you so much." : tip === "not as generous" ? "Thank you." : "Bye."
+};
